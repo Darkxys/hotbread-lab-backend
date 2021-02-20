08a755e9d47691ee721617d1767d13342c537708
Darkxys <paifra1@gmail.com>
2021-02-19 22:30:44 -0500

    test to send a message to frontend

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-app= Flask(__name__)
+from flask import Flask, request, jsonify
+app= Flask(__name__) 
+
 @app.route('/')
 def index():
-  return "<h1>Welcome to Coding OOF</h1>"
+  return jsonify("Welcome to Coding OOF")
